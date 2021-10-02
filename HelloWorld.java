a9f67c34753893b7ea9b20aff86948f6a3612be8
syedTshah22 <87163091+syedTshah22@users.noreply.github.com>
2021-10-02 16:53:42 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World! I am Syed Tallal Shah, and my goal is to have my name in the history books someday. It is not enough to be succesful, I want to be great.");
     }
 }
